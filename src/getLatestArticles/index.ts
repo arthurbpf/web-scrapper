990fafb9867732f6220b0b05ed4d0c4215ecb091
Arthur Fernandes <arthurbpfernandes@gmail.com>
2021-05-23 16:08:39 -0300

    Add timestamp to route

--- src/getLatestArticles/index.ts
+++ src/getLatestArticles/index.ts
@@ -5,10 +5,12 @@ import api from '../api';
 
 interface Article {
 	title: string;
+	linkToAuthor: string;
 	linkToArticle: string;
 	thumbnailImage: string;
 	author: string;
 	datePublished: Date;
+	timestamp: Date;
 }
 
 export default async function getLatestArticles(req: Request, res: Response) {
@@ -21,12 +23,17 @@ export default async function getLatestArticles(req: Request, res: Response) {
 			linkToArticle:
 				(process.env.BASE_URL || '') + $(e).find('h4 a').prop('href'),
 			thumbnailImage: $(e).find('img').prop('src'),
-			author: $(e).find('div.mis-text-sans span:first-of-type').text(),
+			author: $(e).find('div.mis-text-sans a').text(),
+			linkToAuthor:
+				(process.env.BASE_URL || '') +
+				'/' +
+				$(e).find('div.mis-text-sans a').prop('href'),
 			datePublished: parse(
 				$(e).find('div.mis-text-sans span:first-of-type').text(),
 				'dd/MM/yyyy',
 				new Date(0, 0, 0, 0),
 			),
+			timestamp: new Date(),
 		};
 	});
 
