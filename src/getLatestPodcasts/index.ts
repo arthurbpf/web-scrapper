627cd682e18e3d2d9a0c75f4a44b705fd3b0c6fb
Arthur Fernandes <arthurbpfernandes@gmail.com>
2021-05-23 16:10:32 -0300

    Implement Podcasts route

---
+++ src/getLatestPodcasts/index.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express';
+import { parse } from 'date-fns';
+import cheerio from 'cheerio';
+import api from '../api';
+
+interface Podcast {
+	title: string;
+	linkToPodcast: string;
+	description?: string;
+	thumbnailImage: string;
+	datePublished: Date;
+	timestamp: Date;
+}
+
+export default async function getLatestPodcasts(req: Request, res: Response) {
+	const $ = cheerio.load((await api.get('Podcasts.aspx')).data);
+
+	let podcasts: Podcast[] = [];
+
+	$('ul.media-list li').each((i, e) => {
+		podcasts[i] = {
+			title: $(e).find('h4 a').text().replace('\n', '').trim(),
+			linkToPodcast:
+				(process.env.BASE_URL || '') +
+				'/' +
+				$(e).find('h4 a').prop('href'),
+			thumbnailImage: $(e).find('img').prop('src'),
+			datePublished: parse(
+				$(e).find('div span:first-of-type.mis-text-sans').text(),
+				'dd/MM/yyyy',
+				new Date(0, 0, 0, 0),
+			),
+			timestamp: new Date(),
+		};
+	});
+
+	podcasts = await Promise.all(
+		podcasts.map(async (podcast) => {
+			try {
+				const $podcastPage = cheerio.load(
+					(await api.get(podcast.linkToPodcast)).data,
+				);
+
+				podcast.description = $podcastPage('div.mis-text p').text();
+			} catch (error) {
+				podcast.description = 'No description found';
+			} finally {
+				return podcast;
+			}
+		}),
+	);
+
+	if (req.query?.description) {
+		podcasts = podcasts.filter((podcast) =>
+			podcast.description?.includes(req.query.description),
+		);
+	}
+
+	res.json(podcasts);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import getLatestArticles from './getLatestArticles';
+import getLatestPodcasts from './getLatestPodcasts';
 
 if (process.env.NODE_ENV !== 'production') {
 	dotenv.config();
@@ -9,6 +10,7 @@ if (process.env.NODE_ENV !== 'production') {
 const server = express();
 
 server.get('/latest-articles', getLatestArticles);
+server.get('/latest-podcasts', getLatestPodcasts);
 
 server.listen(process.env.PORT || 3333, () =>
 	console.log('Server started! 🏁'),
